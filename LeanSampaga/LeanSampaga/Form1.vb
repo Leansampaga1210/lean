83385c1c11bb6647b91896528629faa4154a4dfe
RonColosa <roncolosa3@gmail.com>
2023-06-01 12:57:07 +0800

    initial

---
+++ LeanSampaga/LeanSampaga/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.Appstart = "Trial Version" Then
+
+            If My.Settings.LoadCount < 0 Then
+                Form2.ShowDialog()
+            End If
+
+            Me.Text = My.Settings.Appstart & ":Counter=>
+                " & My.Settings.LoadCount
+            My.Settings.LoadCount -= 1
+            My.Settings.Save()
+        Else
+            Me.Text = My.Settings.Appstart
+
+        End If
+    End Sub
+End Class
---
+++ SampagaLean/SampagaLean/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.Appstat = "Trial Version" Then
+
+            If My.Settings.Loadcount < 0 Then
+                Form2.ShowDialog()
+            End If
+
+            Me.Text = My.Settings.Appstat & " : Counter =>
+                " & My.Settings.Loadcount
+            My.Settings.Loadcount -= 1
+            My.Settings.Save()
+
+        Else
+            Me.Text = My.Settings.Appstat
+
+        End If
+    End Sub
+End Class
